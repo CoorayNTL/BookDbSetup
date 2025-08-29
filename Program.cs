e89cbfbca1d39414a74ae50e559be9b242834165
CoorayNTL <tl685998@gmail.com>
2025-08-29 17:04:41 +0530

    Initial commit updated with structure of project(Q2)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var context = new AppDbContext();
+        await context.Database.MigrateAsync(); 
+        var authors = await context.Authors.ToListAsync();
+        Console.WriteLine($"Seeded {authors.Count} authors.");
+
+        context.Genres.Add(new Genre { Id = 1, Name = "Fantasy" });
+        await context.SaveChangesAsync();
+        Console.WriteLine("Migration applied and verified.");
+    }
+}
